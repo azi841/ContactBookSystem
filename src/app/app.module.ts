1f625bb2ff8b1d1341894db1d45a6a1eb2c20a38
amehic <aazzhhaarroo@gmail.com>
2022-07-08 16:54:14 +0200

    Component and module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
