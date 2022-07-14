15767ce5528fe06f16a1f078a5851afd336379ab
amehic <aazzhhaarroo@gmail.com>
2022-07-14 15:43:48 +0200

    contact page component

--- src/app/contact-page/contact-page.component.spec.ts
+++ src/app/contact-page/contact-page.component.spec.ts
@@ -1,25 +1,30 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContactPageComponent } from './contact-page.component';
+
+import { ContactData } from '../state/contact.model'
+
+
 
 describe('ContactPageComponent', () => {
   let component: ContactPageComponent;
   let fixture: ComponentFixture<ContactPageComponent>;
+ 
 
-  beforeEach(async () => {
+
+  beforeEach(async()=>{
     await TestBed.configureTestingModule({
-      declarations: [ ContactPageComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      imports: [],
+      declarations: [ContactPageComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
     fixture = TestBed.createComponent(ContactPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
+
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import { ContactData } from '../state/contact.model';
+import { select, Store } from '@ngrx/store';
+import { addContacts } from '../state/contact.actions';
+import { selectContacts } from '../state/contact.selector';
+import { ContactState } from '../state/contact.reducers';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss']
 })
-export class ContactPageComponent implements OnInit {
+export class ContactPageComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  isShown: boolean[] = [] ; 
+  
+  contact: ContactData = {fname:'', lname:'', phone:'', email:'', address: ''}
+
+  contacts$: Observable<ContactData[]>;
+
+  constructor(private store: Store<ContactState>) { 
+    this.contacts$ = this.store.pipe(select(selectContacts))
+  }
+
+  addContact(): void {
+    
+        this.store.dispatch(addContacts(this.contact));
+        this.contact = {fname:'', lname:'', phone:'', email:'', address: ''}
+  }
+
+  
+  toggleShow(i: any) {
+    
+        if(this.isShown[i] == null){
+          this.isShown[i] == false;
+        }
+        this.isShown[i] = ! this.isShown[i];
   }
 
 }
