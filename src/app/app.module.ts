1fc56383db2a0c06f1958a894441a8eb9f456aaa
amehic <aazzhhaarroo@gmail.com>
2022-07-14 15:47:52 +0200

    app component and app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { contactFeatureKey, reducer } from './state/contact.reducers';
+import { environment } from 'src/environments/environment';
+import { StoreDevtools, StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ContactModule } from './contact/contact.module';
+import { reducers, metaReducers } from './reducer'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactPageComponent
+    AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot(reducers, {metaReducers}),
+    ContactModule
   ],
-  providers: [],
+  exports: [ContactPageComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
